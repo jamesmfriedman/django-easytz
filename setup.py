aa32934067b75e76e286d6be810e03f792d590e2
James Friedman <jamesmfriedman@gmail.com>
2013-07-15 14:00:51 -0400

    project name was taken...

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-tz',
+    name = 'django-easytz',
     version = '0.1',
     description = 'Timezone localization without any thinking or doing whatsoever.',
     long_description = open('README.md').read(),
     author = 'James Friedman',
     author_email = 'jamesmfriedman@gmail.com',
-    url = 'https://github.com/jamesmfriedman/django-timezones/',
-    download_url = 'https://github.com/jamesmfriedman/django-timezones/',
+    url = 'https://github.com/jamesmfriedman/django-easytz/',
+    download_url = 'https://github.com/jamesmfriedman/django-easytz/',
     license = 'Apache',
-    keywords = ('timezone','tzinfo', 'datetime', 'jamesmfriedman', 'time', 'django', 'django-timezones'),
+    keywords = ('timezone','tzinfo', 'datetime', 'jamesmfriedman', 'time', 'django', 'django-timezones', 'django-tz', 'django-easytz'),
     packages = find_packages(),
-    package_data = {'timezones': ['static/timezones/js/*']},
+    package_data = {'easytz': ['static/easytz/js/*']},
     tests_require = (
         'django>=1.4',
     ),
